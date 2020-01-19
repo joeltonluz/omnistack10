60c9c2766403da73744db1f2ad7da644a87c8b0b
Joelton Lino Luz <joelton@live.com>
2020-01-19 17:28:33 -0300

    semana omnistack 10

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const routes = require('./routes');
+const http = require('http');
 
+const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-7s928.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -15,5 +20,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () =>
+server.listen(3333, () =>
   console.log('Server is Running in port 3333, enjoy it !!'));
